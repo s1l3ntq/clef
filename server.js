20438af18197fe22d0f5021b4ed825aa9b6d36da
s1l3ntq <72721010+s1l3ntq@users.noreply.github.com>
2022-11-09 17:55:36 -0600

    Displayed accestoken and client id

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json()); 
+
 
 app.post('/login', (req, res) => {
     const code = req.body.code
@@ -17,7 +22,10 @@ app.post('/login', (req, res) => {
             refreshToken: data.body.refresh_token,
             expiresIn: data.body.expires_in
         })
-    }).catch(() => {
+    }).catch((err) => {
+        console.log(err)
         res.sendStatus(400)
     })
 })
+
+app.listen(3001)
